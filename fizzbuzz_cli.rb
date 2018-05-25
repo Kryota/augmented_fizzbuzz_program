af53f19323f4912c922512d79910f7d40006e2d7
Ryota <k16koyama@gmail.com>
2018-05-25 14:22:33 +0900

    add comment

--- fizzbuzz_cli.rb
+++ fizzbuzz_cli.rb
@@ -1,5 +1,6 @@
 list = ""
 
+# CLIで引数指定
 ARGV.each_index do |i|
   v = ARGV[i]
   # 引数の形を正規表現でチェック
@@ -7,21 +8,21 @@ ARGV.each_index do |i|
     # stringをparseしてarrayからハッシュに変換
     v_array = v.delete(' ').split(/[:,]/)
     v_hash = v_array.each_slice(2).map { |k, v| [k.to_i, v.to_s] }.to_h
-    v_hash.each do |key, value|
-      # 数値かどうかを判断
+    v_hash.each do |key, value| # keyとvalueに分ける
+      # 一番後ろの引数が数値かどうかを判断
       if ARGV[-1] =~ /^[0-9]+$/
-        if (ARGV[-1].to_i % key) == 0
-          list = list + value
+        if (ARGV[-1].to_i % key) == 0 # 数値ならkeyの数値で割り切れるか判断
+          list = list + value # 割り切れたら文字列を後ろにappend
           break
         else
-          list = key
+          list = key # 割り切れなかったらそのまま数値を代入
           break
         end
       else
-        list = "Invalid Input"
+        list = "Invalid Input" # 数値じゃないならエラー
       end
     end
-  elsif v !=~ /^[0-9]+\:.*$/ && i != (ARGV.length - 1)
+  elsif v !=~ /^[0-9]+\:.*$/ && i != (ARGV.length - 1) # 引数が指定の形じゃない
     list = "Invalid Input"
   elsif v !=~ /^[0-9]+\:.*$/ && i == (ARGV.length - 1)
     if list.to_s.include?("Invalid Input")
